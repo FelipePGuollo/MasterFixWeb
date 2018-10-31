1174c7a090a7ec372c08151e9aec90fa59bbaec1
felipe <lfelipezin97@gmail.com>
2018-10-31 16:49:01 -0300

    Commits

---
+++ MasterFixWeb/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterFixWeb.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+    }
+}
